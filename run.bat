b1ce2b6b2d6910fa16f21e49cfb4f4cece3c0420
R3CI <extiverooo@proton.me>
2025-07-02 10:42:05 +0200

    venv to run

--- run.bat
+++ run.bat
@@ -1,3 +1,21 @@
 @echo off
-py main.py
+setlocal
+
+if not exist "venv" (
+    python -m venv venv
+)
+
+REM 
+call venv\Scripts\activate.bat
+
+REM
+python -m pip install --upgrade pip
+
+REM 
+pip install -r requirements.txt
+
+REM
+python main.py
+
+endlocal
 pause
